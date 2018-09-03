b023e49febada408e126129e9931dcd0b3ae1ed2
JJ Jiang <jj252@duke.edu>
2018-09-03 11:47:06 -0400

    completed totality

--- src/Totality.java
+++ src/Totality.java
@@ -1,4 +1,26 @@
 
 public class Totality {
-
+	public int sum(int[] a, String stype) {
+		int sum = 0;
+		
+		if (stype.equals("odd")) {
+			for (int i = 1; i < a.length; i +=2) {
+				sum = sum + a[i];
+			}
+		}
+		
+		if (stype.equals("even")) {
+			for (int i = 0; i < a.length; i +=2) {
+				sum = sum + a[i];
+			}
+		}
+		
+		if (stype.equals("all")) {
+			for (int i = 0; i < a.length; i +=1) {
+				sum = sum + a[i];
+			}
+		}
+		
+		return sum;
+	}
 }
